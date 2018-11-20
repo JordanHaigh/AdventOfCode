cc01ef2c1cac497155b392ae225ec3853f175151
jordanhaigh <jhaigh1997@gmail.com>
2018-11-20 20:30:18 +1100

    Part 1 done

--- 2015/Day 2/src/Dimensions.java
+++ 2015/Day 2/src/Dimensions.java
@@ -18,4 +18,27 @@ public class Dimensions {
         this.width = width;
         this.height = height;
     }
+
+    public int findSmallestDimension(){
+        int firstDimension = (length*width);
+        int secondDimension = (width*height);
+        int thirdDimension = (height*length);
+
+        if(firstDimension <= secondDimension && firstDimension <= thirdDimension) return firstDimension;
+        if(secondDimension <= firstDimension && secondDimension <= thirdDimension) return secondDimension;
+        if(thirdDimension <= firstDimension && thirdDimension <= secondDimension) return thirdDimension;
+
+        return firstDimension;//all are equal
+    }
+
+    public int calculateSurfaceArea(){
+        //2*l*w + 2*w*h + 2*h*l
+        return (2*length*width) + (2*width*height) + (2*height*length);
+    }
+
+    public int totalPaperRequiredForDimensions(){
+        int total = calculateSurfaceArea();
+        total += findSmallestDimension();
+        return total;
+    }
 }
--- 2015/Day 2/src/Program.java
+++ 2015/Day 2/src/Program.java
@@ -4,11 +4,15 @@ public class Program {
 
     public static void main(String[]args){
         String filePath = null;
-        filePath  = args.length == 0 ? "wrappingPaperData.txt": args[0];
+        filePath  = args.length == 0 ? "src/wrappingPaperData.txt": args[0];
 
         DataParser dataParser = new DataParser();
         List<Dimensions> allDimensions = dataParser.readFile(filePath);
 
-
+        int totalWrappingPaper = 0;
+        for(Dimensions d : allDimensions){
+            totalWrappingPaper += d.totalPaperRequiredForDimensions();
+        }
+        System.out.println(totalWrappingPaper);
     }
 }
